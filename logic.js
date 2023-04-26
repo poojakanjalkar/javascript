d39199c0447e72b591f0eada3a27c8d4b995b596
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2023-04-27 04:10:25 +0530

    js_pattern

---
+++ logic.js
@@ -0,0 +1,13 @@
+console.log("create pattern")
+let test = () => {
+
+  for (let i = 0; i < 5; i++) {
+    let s =""
+    for (let j = 0; j <= i-1; j++) {
+      s +="*"
+    }
+    console.log(s)
+  }
+}
+
+test()
