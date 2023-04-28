520cf58838fa402b755f77b95d28fde330426832
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2023-04-28 05:40:06 +0530

    primitive data types

---
+++ primitive.js
@@ -0,0 +1,10 @@
+//primitive datatypes javascript nn ss bb u
+let a=null; //null
+let b= 345; //int
+let c=true; //boolean
+let d=BigInt("567") //bigInt
+let e="pooja";  //String
+let f=Symbol("here is special symbol"); //symbol
+let g //undefined
+console.log(a,b,c,d,e,f,g)
+console.log(typeof c);
