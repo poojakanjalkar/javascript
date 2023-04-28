6c6af6442ae528e19925e3ddf55d489100c3503e
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2023-04-28 07:04:00 +0530

    practice set

---
+++ set.js
@@ -0,0 +1,20 @@
+//practice set add number to string
+let a="Pavan"+1;
+console.log(a);
+console.log(typeof(a)); //get type of a
+
+//create const object 
+const a1={
+  name: "pooja",
+  section:1
+}
+console.log(a1.name);
+
+//create dictionary in javascript
+const a2={
+   reticent:"calm",
+   termagent:"aggressive lady",
+   yakka:"hard work",
+   panacea:"medicine of all diseases",
+   } 
+console.log(a2.termagent);
