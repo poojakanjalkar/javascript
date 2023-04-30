d7bcfc19d2c70503c366013d3a40dd13f7b40f8e
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2023-05-01 02:06:45 +0530

    object operations implemented

---
+++ object.js
@@ -0,0 +1,20 @@
+const person={
+  firstName:"Pavan",
+  lastName:"Adhav",
+  age:31,
+  eyeColor:"brown",
+  city:"Pune",
+  isActive:true
+}
+
+let items=["pooja","shreya","Divya","prisha","siya"];
+let lstname=["adhav","yede","pawar","tru","diwan"];
+let age=[22,21,34,23,21];
+let color=["blue","brown","pink","yellow","red"];
+let city=["pune","nanded","yavatmal","nagpur","mumbai"];
+let active=[true,false];
+let n=1.0;
+console.log(items[Math.floor(Math.random()* items.length)])
+// console.log(Math.random())
+//console.log(person);
+
