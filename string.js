65dfa0235ba6fd88dc26a38d503bfd2cb02e1a3b
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2023-05-01 00:25:23 +0530

    string function added

---
+++ string.js
@@ -0,0 +1,9 @@
+let a="pooja";
+console.log("concatenation--->",a.concat("pavan"));
+console.log("Uppercase--->",a.toUpperCase())
+console.log("trim-->",a.trim()+"pavan")
+console.log("array--->",Array.from(a));
+
+console.log("split---->",a.split(""));
+console.log("slice--->",a.slice(2,4))
+
