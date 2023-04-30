ab31733f7c417da4919b764f23f00dec3beea7ab
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2023-05-01 00:37:30 +0530

    added function writing

---
+++ functions.js
@@ -0,0 +1,18 @@
+function printEvenNumber(x){
+  /*for(let i=0;i<=x;i++){
+    if(i%2==0){
+      console.log("even number",i);
+    }
+  }*/
+}
+
+const printEvenNumber1 =(x)=>{
+  for(let i=0;i<=x;i++){
+    if(i%2==0){
+      console.log("even number",i);
+    }
+  }
+}
+
+
+printEvenNumber1(20)
